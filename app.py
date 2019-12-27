363e192e26d9c1dba04705872751f11b5af87964
Lukas Werner <55150634+lwerner-lshigh@users.noreply.github.com>
2019-12-27 12:24:08 -0800

    hahahaha first

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, escape, request
+from gpiozero import Servo
+
+
+app = Flask(__name__)
+servo = Servo(4)
+
+def setAngle(angle):
+    servo.value = angle
+
+@app.route('/open', methods=['POST'])
+def open():
+    print("opening...")
+    return "Sucsess on opening!"
+
+@app.route('/close', methods=['POST'])
+def close():
+    print("closing...")
+    return "Sucsess on closing!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
